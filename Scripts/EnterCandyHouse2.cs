a6eceb5a5e2572549f322294e3cd6c0296d9e11b
Amber Olsen <amcmains@ucsd.edu>
2026-08-14 15:28:42 -0700

    enter candy house2

---
+++ Scripts/EnterCandyHouse2.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EnterCandyHouse2 : MonoBehaviour
+{
+    public CameraMovement myCamera;
+    public bool relocate = false;
+    public GameObject elf;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        myCamera.mode = CameraMode.LockedPosition;
+        myCamera.LockToPosition(new Vector3(-20, 20, -10));
+        relocate = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        relocate = false;
+    }
+
+    void Update()
+    {
+        if (relocate && Keyboard.current.spaceKey.isPressed)
+        {   
+            elf.transform.position = new Vector3(0f, 7f, 0);
+            myCamera.ResumeFollow();
+            relocate = false;
+        }
+    }
+}
